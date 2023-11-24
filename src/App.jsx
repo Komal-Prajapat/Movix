89cce0fbf6292c25f6ae34d3a5c84d204b65ed2e
komalcod <128589948+komalcod@users.noreply.github.com>
2023-11-24 12:59:32 +0530

    home page completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router , Routes , Route} from "react-router-dom"
 // import './App.css'
 import Header from './components/header/Header';
 import Home from './pages/home/Home';
+import './App.css'
 function App() {
 
 
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,52 +1,57 @@
-import React, { useEffect, useState } from 'react'
-import "./home.css"
-import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import React, { useEffect, useState } from 'react';
+import './home.css';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
 import { Link } from 'react-router-dom';
-import typing 
-const home = () => {
 
-const [popularMovies,setpopularMovies]=useState([])
+const Home = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
 
-  useEffect(()=>{
-    fetch("https://api.themoviedb.org/3/discover/movie?api_key=fbeff8af1c622fdf582364494c266f5e")
-    .then(res =>res.json())
-    .then(data=> setpopularMovies(data.results))
-  },[])
+  useEffect(() => {
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=fbeff8af1c622fdf582364494c266f5e')
+      .then((res) => res.json())
+      .then((data) => setPopularMovies(data.results));
+  }, []);
+
   return (
     <>
-    <div className="poster">
-    <Carousel
-     showThumbas={false}
-    autoPlay={true}
-    trasnsitionTime={3}
-    infinteLoop={true}
-    showStatus={false}>
-             {
-              popularMovies.map(movie=>(
-                <Link style={{textDecoration="none" , color:"white"}} to={`/movie/${movie.id}`}>
-                <div className="posterImage">
-                  <img src={`https://image.tmdb.org/t/p/original${movie&&movie.backdrop_path}`}></img>
+      <div className="poster">
+        <Carousel
+          showThumbs={false}
+          autoPlay={true}
+          transitionTime={3}
+          infiniteLoop={true}
+          showStatus={false}
+        >
+          {popularMovies.map((movie) => (
+            <Link
+              key={movie.id}
+              style={{ textDecoration: 'none', color: 'white' }}
+              to={`/movie/${movie.id}`}
+            >
+              <div className="posterImage">
+                <img
+                  src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`}
+                  alt={movie.original_title}
+                />
+              </div>
+              <div className="posterImage_overlay">
+                <div className="posterImage_title">{movie ? movie.original_title : ''}</div>
+                <div className="posterImage_runtime">
+                  {movie ? movie.release_date : ' '}
+                  <span className="posterImage__rating">
+                    {movie ? movie.vote_average : ''}
+                    <i className="fas fa-star"></i>
+                  </span>
                 </div>
-                <div className="poster_mage_overlay">
-                    <div className="poster_Image__title"{movie?movie.original_title:""}>
-                    <div className="posterImage_runtime">
-                      {movie? movie.release_date :""}
-                      <span className=''></span>
-                    </div>
-
-                    </div>
-                </div>
-                </Link>
-
-              
-              ))
-             }
-
-    </Carousel>
+                <div className="posterImage__description">{movie ? movie.overview : ''}</div>
+              </div>
+            </Link>
+          ))}
+        </Carousel>
       </div>
-     </>
-  )
-}
+    </>
+  );
+};
 
-export default home
+export default Home;
